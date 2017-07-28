d9371050eb5a6e4ec22b89dc0a3e04d39427cbbe
tkabeee <tkabee@gmail.com>
2017-07-28 15:50:18 +0900

    controller関連のファイル追加

---
+++ application/controllers/mobile/ErrorController.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'Sdx/Controller/Action/Http.php';
+
+class Mobile_ErrorController extends Sdx_Controller_Action_Http
+{
+  public function indexAction()
+  {
+    $helper = new Sdx_Controller_Action_Helper_ErrorRenderer();
+    $this->addHelper($helper);
+
+    if ($handler = $this->_getParam('error_handler'))
+    {
+      $this->_helper->errorRenderer();
+
+      $module = $this->_getParam('module');
+      $code = ($handler->exception->getCode() == '500') ? '500' : '404';
+      $template = $module.'/error/'.$code.'.tpl';
+      $this->setViewRendererPath($template);
+    }
+    else
+    {
+      $this->_helper->errorRenderer();
+    }
+  }
+}
